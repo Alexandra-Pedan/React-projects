799af1b08f28e059bed47465a86f203816e72571
Aleksandra Pedan <sashka28465@gmail.com>
2023-07-06 14:11:04 -0400

    tabs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.scss'
 // import ToursSection from 'components/ToursSection/ToursSection'
 // import Reviews from 'components/Reviews/Reviews'
 // import Accordion from 'components/Accordion/Accordion'
-import MenuSection from 'components/Menu/MenuSection'
+// import MenuSection from 'components/Menu/MenuSection'
+import Tabs from 'components/Tabs/Tabs'
 function App() {
   return (
     <div className="container">
@@ -11,7 +12,8 @@ function App() {
       {/* <ToursSection /> */}
       {/* <Reviews /> */}
       {/* <Accordion /> */}
-      <MenuSection />
+      {/* <MenuSection /> */}
+      <Tabs />
     </div>
   )
 }
